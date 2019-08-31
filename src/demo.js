1774be3de0362076716c631dcfe0f8e325b01984
prashant kumar <kumarprashant1330@gmail.com>
2019-08-31 12:25:23 +0530

    react basics

--- src/demo.js
+++ src/demo.js
@@ -5,10 +5,10 @@ import 'tachyons';
 
 const Demo=(props)=>{
       return( 
-      <div className="avatar dib ma4 bg-light-blue  ">
-         <img src="https://joeschmoe.io/api/v1/prashant " />
+      <div className="avatar dib ma4 bg-light-blue  grow shadow-4">
+         <img src={`https://joeschmoe.io/api/v1/${props.name}` }/>
           <h1 className="tc" >{props.name}</h1>
-          <p className="tc"  >{props.work}</p>
+          <p className="tc"  >{props.profession}</p>
       </div>
       )
    }
--- src/index.js
+++ src/index.js
@@ -4,18 +4,21 @@ import './index.css';
 import App from './App';
 import Demo from './demo'
 import * as serviceWorker from './serviceWorker';
+ var addAvatar= document.getElementById('submit')
+addAvatar.addEventListener("click",(event)=>{
+    var fname=document.getElementById('fname').value;
+var work=document.getElementById('work').value;
+    ReactDOM.render(<div>
+        <Demo id="1" name={`${fname}`} profession={`${work}`}/></div>, document.getElementById('root'));
 
-ReactDOM.render(<div>
-    <Demo id="1" name="prashnat"  work="web devleoper"/>
-    <Demo/>
-    <Demo/>
-    <Demo/></div>
 
-    
-    , document.getElementById('root'));
+})
+
+
 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
