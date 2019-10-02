77963ab6b70377211d19bb6698caeb9a24dcd365
prashant kumar <kumarprashant1330@gmail.com>
2019-10-02 22:19:27 +0530

    classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 class Counter extends Component {
+    
     state = { 
         count: 1,
         imageUrl: 'https://picsum.photos/500'
      };
+     add(){
+         console.log("hello")
+     }
     render() { 
         return (<div>
-        <img src={this.state.imageUrl} />
-        <span>{this.formatCount()}</span>
-        <p> i m react</p>
+        
+        <span className={this.getclasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm" id="add" onClick={this.add} >Increment</button>
         </div> );
     }
     formatCount(){
           const{count}=this.state;
         return  count===0 ? "zero":count;
+    }
+    getclasses(){
+        let classes="badge m-2 badge-";
+        classes+= this.state.count===0 ? "warning" : "primary";
+        return classes;
     }
 }
  
